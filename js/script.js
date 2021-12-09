dba098f7dff648198af4b49db37c3ec8baea391a
ihsansfd <ihsannurul@hotmail.com>
2021-12-09 13:16:09 +0700

    update dikit tombol submit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-var offset = 79;
+// var offset = 79;
 
-$('.navbar li a').click(function(event) {
-    event.preventDefault();
-    $($(this).attr('href'))[0].scrollIntoView();
-    scrollBy(0, -offset);
-});
+// $('.navbar li a').click(function(event) {
+//     event.preventDefault();
+//     $($(this).attr('href'))[0].scrollIntoView();
+//     scrollBy(0, -offset);
+// });
+
+
+function displayMessage() {
+    alert("This is just for demonstrating purpose :) as Github only supports CSS & JavaScript \n\nSend to ihsannurul@hotmail.com if want to contact me");
+}
+
+document.getElementById("button-submit").addEventListener("click", displayMessage);
+const form = document.getElementById("my-form"); 
+function handleForm(event) { event.preventDefault();} 
+form.addEventListener('submit', handleForm);
