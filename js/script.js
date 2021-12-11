7aa1543e4f9b064b6ae4dea3b9dbce4d92ff99ce
ihsansfd <ihsannurul@hotmail.com>
2021-12-11 21:58:13 +0700

    h

--- js/script.js
+++ js/script.js
@@ -1,12 +1,3 @@
-// var offset = 79;
-
-// $('.navbar li a').click(function(event) {
-//     event.preventDefault();
-//     $($(this).attr('href'))[0].scrollIntoView();
-//     scrollBy(0, -offset);
-// });
-
-
 function displayMessage() {
     alert("This is just for demonstrating purpose :) as Github only supports CSS & JavaScript \n\nSend to ihsannurul@hotmail.com if want to contact me");
 }
@@ -16,52 +7,44 @@ const form = document.getElementById("my-form");
 function handleForm(event) { event.preventDefault();} 
 form.addEventListener('submit', handleForm);
 
-      document.addEventListener("DOMContentLoaded", () => {
-        let options = {
-          root: null,
-          rootMargin: "-60px 0px",
-          threshold: 0.05
-        };
-        let observer = new IntersectionObserver(beTouching, options);
-        document.querySelectorAll(".progress").forEach(p => {
-          observer.observe(p);
-          //console.log("watching", p.textContent);
-        });
-      });
+document.addEventListener("DOMContentLoaded", () => {
+let options = {
+  root: null,
+  rootMargin: "-60px 0px",
+  threshold: 0.05
+};
+let observer = new IntersectionObserver(beTouching, options);
+document.querySelectorAll(".progress").forEach(p => {
+  observer.observe(p);
+});
+});
 
-     function handleBeTouching(v) {
-        let i = 0;
-              if (i == 0) {
-                i = 1;
-                let nilai = 1;
-                const widthDefault = v.style.width.replace(/\%/g, "");
-                const nilaiDefault = v.getAttribute("data-nilai");
+function handleBeTouching(v) {
 
-                let id = setInterval(frame, 100);
-                function frame() {
-                  if (nilai >= nilaiDefault ) {
-                    clearInterval(id);
-                    i = 0;
-                  } else {
-                    nilai++;
-                    v.style.width = nilai*10 + "%";
-                    v.innerHTML = nilai  + "/10";
-                  }
-            }
-        }
-    }
-                  
+    let nilai = 1;
+    const nilaiDefault = v.getAttribute("data-nilai");
+    let id = setInterval(frame, 100);
 
-      function beTouching(entries, ob) {
-        //entries all 30 paragraphs
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-                console.log("inersecting");
-                let progressBar = entry.target.children[0];
-                console.dir(progressBar);
-                setTimeout(() => handleBeTouching(progressBar),500);
-                ob.unobserve(entry.target);
-
+    function frame() {
+      if (nilai >= nilaiDefault ) {
+        clearInterval(id);
+      } else {
+        nilai++;
+        v.style.width = nilai*10 + "%";
+        v.innerHTML = nilai  + "/10";
           }
-        });
-      }
+    }
+}
+
+          
+
+function beTouching(entries, ob) {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+            let progressBar = entry.target.children[0];
+            setTimeout(() => handleBeTouching(progressBar),500);
+            ob.unobserve(entry.target);
+
+  }
+});
+}
