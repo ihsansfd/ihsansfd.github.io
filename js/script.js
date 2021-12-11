193b688c44cabf534fedf7171ba88e3e7cdd5006
ihsansfd <ihsannurul@hotmail.com>
2021-12-11 20:03:30 +0700

    edit animasi spesialisasi

--- js/script.js
+++ js/script.js
@@ -15,3 +15,53 @@ document.getElementById("button-submit").addEventListener("click", displayMessag
 const form = document.getElementById("my-form"); 
 function handleForm(event) { event.preventDefault();} 
 form.addEventListener('submit', handleForm);
+
+      document.addEventListener("DOMContentLoaded", () => {
+        let options = {
+          root: null,
+          rootMargin: "-60px 0px",
+          threshold: 0.05
+        };
+        let observer = new IntersectionObserver(beTouching, options);
+        document.querySelectorAll(".progress").forEach(p => {
+          observer.observe(p);
+          //console.log("watching", p.textContent);
+        });
+      });
+
+     function handleBeTouching(v) {
+        let i = 0;
+              if (i == 0) {
+                i = 1;
+                let nilai = 1;
+                const widthDefault = v.style.width.replace(/\%/g, "");
+                const nilaiDefault = v.getAttribute("data-nilai");
+
+                let id = setInterval(frame, 100);
+                function frame() {
+                  if (nilai >= nilaiDefault ) {
+                    clearInterval(id);
+                    i = 0;
+                  } else {
+                    nilai++;
+                    v.style.width = nilai*10 + "%";
+                    v.innerHTML = nilai  + "/10";
+                  }
+            }
+        }
+    }
+                  
+
+      function beTouching(entries, ob) {
+        //entries all 30 paragraphs
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+                console.log("inersecting");
+                let progressBar = entry.target.children[0];
+                console.dir(progressBar);
+                setTimeout(() => handleBeTouching(progressBar),500);
+                ob.unobserve(entry.target);
+
+          }
+        });
+      }
